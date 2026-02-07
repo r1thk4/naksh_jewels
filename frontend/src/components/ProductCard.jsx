5e561f7f040de0057f9c10c048b97f133d5f626a
r1thk4 <rithikathilakan@gmail.com>
2026-02-07 15:43:12 +0530

    Added view cart button

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,19 +1,41 @@
 import { useCart } from "../context/CartContext";
+import { Link } from "react-router-dom";
 
 const ProductCard = ({ product }) => {
   if (!product) return null;
 
-  const { addToCart } = useCart();
+  const { cartItems, addToCart } = useCart();
+
+  const isInCart = cartItems.some(
+    (item) => item.id === product.id
+  );
 
   return (
     <div className="editorial-card">
-      <img src={product.image} alt={product.name} className="product-image" />
+      <img
+        src={product.image}
+        alt={product.name}
+        className="product-image"
+      />
+
       <div className="editorial-info">
         <h4>{product.name}</h4>
         <p>₹{product.price}</p>
-        <button onClick={() => addToCart(product)}>
-          Add to Cart
-        </button>
+
+        {isInCart ? (
+          <Link to="/cart">
+            <button className="primary-btn">
+              View Cart
+            </button>
+          </Link>
+        ) : (
+          <button
+            className="primary-btn"
+            onClick={() => addToCart(product)}
+          >
+            Add to Cart
+          </button>
+        )}
       </div>
     </div>
   );
